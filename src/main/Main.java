ed0c2878389a6f5f0cf773741943360ca0cd8b84
ijayantsingh <jayant.singh_cs18@gla.ac.in>
2020-02-24 21:02:26 +0530

    made an Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,7 +15,7 @@ public class Main {
         String emailOfContact = new String();
         boolean ctr;
         while (ctr = true) {
-            System.out.println("Welcome to Shivam's Contact List App");
+            System.out.println("Welcome to Jayant's Contact List App");
             System.out.println("Press 1 to add a new contact\n" + "Press 2 to view all contacts\n" +
                     "Press 3 to search for a contact\n" + "Press 4 to delete a contact\n" + "Press 5 to exit program ");
 
