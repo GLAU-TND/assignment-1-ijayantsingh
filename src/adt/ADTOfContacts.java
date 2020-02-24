0a52800ee73ba7d38aa0a93fcbc74c7e70ec1f0c
ijayantsingh <jayant.singh_cs18@gla.ac.in>
2020-02-24 20:59:35 +0530

    made an definition classes

--- src/adt/ADTOfContacts.java
+++ src/adt/ADTOfContacts.java
@@ -1,15 +1,14 @@
 package adt;
 
 public interface ADTOfContacts<E> {
-    //for Adding contacts
+
     boolean addContact(E item);
-    //for viewing Contacts
+
     void viewContacts();
-    //for Searching contacts from the Contact List
+
     void searchInContacts(String name);
-    //for Deleting any contact from Contact List
+
     boolean deleteFromContacts(int index);
-    //for Printing Contacts from Contact List
-    // void printContact();
+
 
 }
